a4d23eaf0d40bac0a13e02cab642508d6b686885
Carl Zulauf <carl@linkleaf.com>
2012-10-11 00:18:57 -0400

    State FIPS codes are never used. Removed.

--- lib/street_address.rb
+++ lib/street_address.rb
@@ -512,64 +512,6 @@ module StreetAddress
 
     STATE_NAMES = STATE_CODES.invert
     
-    STATE_FIPS = {
-      "01" => "AL",
-      "02" => "AK",
-      "04" => "AZ",
-      "05" => "AR",
-      "06" => "CA",
-      "08" => "CO",
-      "09" => "CT",
-      "10" => "DE",
-      "11" => "DC",
-      "12" => "FL",
-      "13" => "GA",
-      "15" => "HI",
-      "16" => "ID",
-      "17" => "IL",
-      "18" => "IN",
-      "19" => "IA",
-      "20" => "KS",
-      "21" => "KY",
-      "22" => "LA",
-      "23" => "ME",
-      "24" => "MD",
-      "25" => "MA",
-      "26" => "MI",
-      "27" => "MN",
-      "28" => "MS",
-      "29" => "MO",
-      "30" => "MT",
-      "31" => "NE",
-      "32" => "NV",
-      "33" => "NH",
-      "34" => "NJ",
-      "35" => "NM",
-      "36" => "NY",
-      "37" => "NC",
-      "38" => "ND",
-      "39" => "OH",
-      "40" => "OK",
-      "41" => "OR",
-      "42" => "PA",
-      "44" => "RI",
-      "45" => "SC",
-      "46" => "SD",
-      "47" => "TN",
-      "48" => "TX",
-      "49" => "UT",
-      "50" => "VT",
-      "51" => "VA",
-      "53" => "WA",
-      "54" => "WV",
-      "55" => "WI",
-      "56" => "WY",
-      "72" => "PR",
-      "78" => "VI"
-    }
-
-    FIPS_STATES = STATE_FIPS.invert
-    
     class << self
       attr_accessor(
         :street_type_regexp,
@@ -835,10 +777,6 @@ module StreetAddress
           self.send("#{attrib}=", args[attrib]) 
         end
         return
-      end
-
-      def state_fips
-        StreetAddress::US::FIPS_STATES[state]
       end
 
       def state_name
