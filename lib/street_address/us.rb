26468f09e014ddb4ccbe83525342786dbd0bfb1d
Carl Zulauf <carl@linkleaf.com>
2012-11-13 13:35:00 -0500

    Refactoring of StreetAddress::US to understand address separators

--- lib/street_address/us.rb
+++ lib/street_address/us.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module StreetAddress
   class US
 
@@ -11,18 +13,19 @@ module StreetAddress
     include StreetTypes
 
     VERSION             = '1.1.0'
-    UNIT_PREFIX_PATTERN = /^(su?i?te|p\W*[om]\W*b(?:ox)?|dept|apt|apartment|ro*m|fl|unit|box|lot)\.?$/i
-    UNIT_PATTERN        = /^([a-z][-\/]?\d*|\d+[-\/]?(\d+|[a-z])?)$/i
+    UNIT_PREFIX_PATTERN = /^(su?i?te|p\W*[om]\W*b(?:ox)?|dept|apt|apartment|ro*m|fl|unit|box|lot|#)\.?$/i
+    UNIT_PATTERN        = /^#?([a-z][-\/]?\d*|\d+[-\/]?(\d+|[a-z])?)$/i
     CORNER_PATTERN      = /^(&|and|at)$/i
     LINE_PATTERN        = /,|[\r\n]{1,2}/
     TOKEN_PATTERN       = /^[A-Za-z0-9'\-&#\.\/]+$/
+    SEPARATOR           = -1
 
 =begin rdoc
 
     parses either an address or intersection and returns an instance of
     StreetAddress::US::Address or nil if the location cannot be parsed
 
-    pass the arguement, :informal => true, to make parsing more lenient
+    pass the argument, :informal => true, to make parsing more lenient
     
 ====example
     StreetAddress::US.parse('1600 Pennsylvania Ave Washington, DC 20006')
@@ -45,17 +48,21 @@ module StreetAddress
 
     def parse(location, options = nil)
       reset location
-      return nil unless @tokens.all?{|t| t =~ TOKEN_PATTERN }
+      return nil unless @tokens.all?{|t| t =~ TOKEN_PATTERN or t == SEPARATOR }
       options = options ? @options.merge(options) : @options
       parse_number
       if @address.number.nil? and intersection?
         parse_intersection
-      elsif @tokens.any?
+      elsif @address.number and @tokens.any?
         parse_address
+      else
+        return nil
       end
       if @address.valid?(options)
         @options[:normalize] == false ? @address : normalize_address(@address)
       end
+    #rescue
+    #  nil
     end
 
   private
@@ -72,9 +79,10 @@ module StreetAddress
       parse_direction_suffix  street
       parse_zip               other if other.any?
       parse_leading_unit      other if other.any?
-      parse_state             other if other.any?
       parse_trailing_unit     street unless @address.unit or street.one?
       parse_street            street
+      other = street if other.empty? and !street.empty?
+      parse_state             other if other.any?
       parse_city              other
     end
 
@@ -84,6 +92,7 @@ module StreetAddress
 
     def parse_state(tokens)
       @address.state = tokens.pop if state?(tokens.last)
+      tokens.pop if tokens.last == SEPARATOR
     end
 
     def parse_zip(tokens)
@@ -91,23 +100,42 @@ module StreetAddress
         @address.postal_code     = $1
         @address.postal_code_ext = $2
         tokens.pop
+        tokens.pop if tokens.last == SEPARATOR
       end
     end
 
     def parse_leading_unit(tokens)
       @address.unit_prefix  = tokens.shift if unit_prefix?(tokens.first)
       @address.unit         = tokens.shift if unit?(tokens.first)
+      tokens.shift if tokens.first == SEPARATOR
     end
 
     def parse_trailing_unit(tokens)
-      @address.unit = tokens.pop if unit?(tokens.last)
-      if @address.unit and unit_prefix?(tokens.last)
-        @address.unit_prefix = tokens.pop
+      if unit_prefix?(tokens[-2]) and unit?(tokens.last)
+        @address.unit         = tokens.pop.match(UNIT_PATTERN)[1]
+        @address.unit_prefix  = tokens.pop
+      elsif tokens.last[0] == "#"
+        @address.unit = tokens.pop.match(UNIT_PATTERN)[1]
       end
+      tokens.pop if tokens.last == SEPARATOR
     end
 
     def parse_street(tokens)
-      @address.street = tokens.join(" ") unless tokens.empty?
+      street = nil
+      if tokens.count > 1
+        (1..(tokens.count - 1)).to_a.reverse.each do |i|
+          if street_types[tokens[i].downcase.gsub(".", "")]
+            street = tokens.slice!(0, i)
+            @address.street_type = tokens.shift
+            break
+          end
+        end
+      end
+      unless street
+        street = tokens.clone
+        tokens.clear
+      end
+      @address.street = street.join(" ") unless street.empty?
     end
 
     def parse_number
@@ -139,38 +167,44 @@ module StreetAddress
     end
 
     def tokenize(str)
-      str.strip.split(/[ ,#\n\r\t]+/)
+      tokens = []
+      pieces = str.strip.split(/ +/)
+      pieces.each_with_index do |piece, i|
+        if i < 1
+          tokens << piece
+        else
+          tokens += separate_piece(piece)
+        end
+      end
+      tokens
+    end
+
+    def separate_piece(piece)
+      if piece =~ /\A(.+?)(\r?\n|,)\z/m
+        separate_piece($1) + [SEPARATOR]
+      else
+        [].tap do |pieces|
+          piece.split(/(?:\r?\n)+/).each_with_index do |t, i|
+            pieces << SEPARATOR if i > 0
+            pieces << t
+          end
+        end
+      end
+    end
+
+    def clean_token(token)
+      token.gsub(".", "")
     end
 
     def split_parts
-      street, other = split_parts_by_street_type
-      if street.nil?
-        street, other = @location.split(LINE_PATTERN, 2).map{|l| tokenize l }
-        street.shift # number already parsed
+      street, other = [], []
+      first_line = true
+      @tokens.each do |t|
+        first_line = false if t == SEPARATOR
+        first_line ? street << t : other << t
       end
-      other ||= []
+      other.shift if other.first == SEPARATOR
       [street, other]
-    end
-
-    def split_parts_by_street_type
-      street_type_index = nil
-      @tokens.each_with_index do |t, i|
-        if i > 0 and street_types[t.downcase.gsub(".","")]
-          street_type_index = i
-          @address.street_type = t
-          next_token = @tokens[i+1]
-          break unless next_token and street_types[next_token.downcase.gsub(".","")]
-        end
-      end
-      if street_type_index
-        street = @tokens[0..(street_type_index - 1)]
-        other  = @tokens[(street_type_index + 1)..-1]
-        @address.suffix = other.shift if other.any? and direction?(other.first)
-        @address.suffix += " #{other.shift}" if other.one? and direction?(other.first)
-        [street, other]
-      else
-        nil
-      end
     end
 
     def intersection?
